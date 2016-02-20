61665df332da68c371d8e484d0763a9b156ec5cf
Rahat Mahbub <rahatm1@uvic.ca>
2016-02-20 13:20:43 -0800

    Init pathfinder

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 var config  = require('../config.json');
 var express = require('express');
 var router  = express.Router();
+var PF = require('pathfinding');
 
 // Handle GET request to '/'
 router.get(config.routes.info, function (req, res) {
   // Response data
-  var data = {
-    color: config.snake.color,
-    head_url: config.snake.head_url,
-  };
+    var data = {
+        color: config.snake.color,
+        head_url: config.snake.head_url,
+    };
 
-  return res.json(data);
+    return res.json(data);
 });
 
 // Handle POST request to '/start'
@@ -18,24 +19,42 @@ router.post(config.routes.start, function (req, res) {
   // Do something here to start the game
 
   // Response data
-  var data = {
-    taunt: config.snake.taunt.start
-  };
+    var data = {
+        taunt: config.snake.taunt.start
+    };
 
-  return res.json(data);
+    return res.json(data);
 });
 
 // Handle POST request to '/move'
 router.post(config.routes.move, function (req, res) {
   // Do something here to generate your move
+    var body = req.body;
+    console.log(JSON.parse(body));
+    var snakes = body.snakes;
+    var myHead;
+    var food = body.food[0];
 
-  // Response data
-  var data = {
-    move: 'north', // one of: ["north", "east", "south", "west"]
-    taunt: config.snake.taunt.move
-  };
+    for (var i = 0; i < snakes.length; i++) {
+        if (config.snake.id === snake[i].id) {
+            myHead = snake[i].coords[0];
+        }
 
-  return res.json(data);
+    }
+
+    var grid = new PF.Grid(body.width, body.height);
+
+    var finder = new PF.AStarFinder(myHead[0], myHead[1], food[0], food[1], grid);
+
+    console.log(matrix);
+
+    // Response data
+    var data = {
+        move: 'north', // one of: ["north", "east", "south", "west"]
+        taunt: config.snake.taunt.move
+    };
+
+    return res.json(data);
 });
 
 // Handle POST request to '/end'
@@ -43,9 +62,9 @@ router.post(config.routes.end, function (req, res) {
   // Do something here to end your snake's session
 
   // We don't need a response so just send back a 200
-  res.status(200);
-  res.end();
-  return;
+    res.status(200);
+    res.end();
+    return;
 });
 
 
