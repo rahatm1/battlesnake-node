4102f2da88cb5dbd4ae692adfd31cce2f5e8f458
Rhiannon <rtullybarr@gmail.com>
2016-02-20 16:10:33 -0800

    print err.stack

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use(function (err, req, res, next) {
 // 500 handler middleware, respond with JSON only
 app.use(function (err, req, res, next) {
   var statusCode = err.status || 500;
-
+  console.log(err.stack);
   res.status(statusCode);
   res.send({
     status: statusCode,
